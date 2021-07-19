e51f7c045cfc06b0b88f080c5c624f95548609d5
BIT <BIT@DESKTOP-DGBBQFQ>
2021-07-19 11:49:33 +0900

    비트 연산자

--- src/com/javaex/basics/OperatorEx.java
+++ src/com/javaex/basics/OperatorEx.java
@@ -3,7 +3,34 @@ package com.javaex.basics;
 public class OperatorEx { 
 		public static void main(String[] args) {
 //				arithOperEx();
-				LogicOperEx();
+//				LogicOperEx();
+				bitOperEx();
+		}
+		
+		//	비트 연산자
+		private static void bitOperEx() {
+				// 	하드웨어 제어, 이미지 처리 등
+				//	미세하게 비트 단위 데이터 제어에 활용
+				byte b1 = 0b1101;
+				byte b2 = 0b0111;
+				
+				System.out.println("b1: " + Integer.toBinaryString(b1));			//	1101
+				System.out.println("b2: " + Integer.toBinaryString(b2));		// 0111
+				
+				int result = b1 & b2;
+				System.out.println("b1 & b2 : " + Integer.toBinaryString(result));		// 0101
+				
+				result = b1 | b2;		//	비트 논리합
+				System.out.println("b1 | b2 : " + Integer.toBinaryString(result));			//	1111
+				
+				result = ~b1;		//	비트 논리 부정
+				System.out.println("~b1 : " + Integer.toBinaryString(result));
+				
+				result = b1 ^ b2;		//	베타적 논리합
+				System.out.println("b1 ^ b2 : " + Integer.toBinaryString(result));
+				
+				
+				
 		}
 		//	비교 연산과 논리 연산
 		private static void LogicOperEx() {
@@ -13,6 +40,38 @@ public class OperatorEx {
 				System.out.println("a > b ?" + (a > b));
 				System.out.println("a와 b가 같습니까? " + (a == b));
 				System.out.println("a와 b가 다릅니까? " + (a != b));
+				
+				
+				//	논리 연산자 : 논리곱(AND:&&), 논리합(OR: ||), 논리부정(NOT : !)
+				int num = 5;
+				
+				//	num: 0초과 10미만의 값인가?
+				//			조건1: num > 0
+				//			조건2: num < 10
+				//	결과: 조건1 and 조건2
+				boolean r1 = num > 0;
+				boolean r2 = num < 10;
+				boolean r1Andr2 = r1 && r2;
+				// num > 0 && num < 10
+				System.out.println("r1 && r2 =>" +r1Andr2);
+				
+				//	num: 0이하이거나 10이상의 값인가?
+				//			조건1 = num <= 0 ?
+				//			조건2 = num > 10 ?
+				// 결과: 조건1 or 조건2 (논리합: 합집합)
+				boolean r3 = num <= 0;
+				boolean r4 = num > 10;
+				boolean r3Orr4 = r3 || r4;
+				// num <= 0 || num > 10
+				System.out.println("r3 || r4 =>" + r3Orr4);
+				
+				//	논리 부정: 
+				// num > 0 && num < 10 -> 논리 부정
+				// num <= 0 || num >= 10
+				
+				boolean rNot = !(num > 0 && num < 10);
+				System.out.println("num가 0 초과, 10미만 이외의 값인가?" +rNot);
+				
 				
 		}
 		//	산술 연산

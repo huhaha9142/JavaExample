58aee432ba01fe7f5f4e2457f7cf22b1ba2d2a85
BIT <BIT@DESKTOP-DGBBQFQ>
2021-07-23 10:48:49 +0900

    oop 수정

--- src/com/javaex/oop/MethodEx.java
+++ src/com/javaex/oop/MethodEx.java
@@ -3,11 +3,16 @@ package com.javaex.oop;
 public class MethodEx {
 
 	public static void main(String[] args) {
-		printMessage();
-		printMessage("Hello, Method");
-		
+//		printMessage();
+//		printMessage("Hello, Method");
+//		printDivide(10, 0);
+		System.out.println(divide(10, 3));
 	}
 	
+	//	입력 o, 출력 o
+	private static double divide(double num1, double num2) {
+		return num1 / num2;
+	}
 	//	입력 o, 출력 x
 	private static void printDivide(int num1, int num2) {
 		if (num2 == 0) {

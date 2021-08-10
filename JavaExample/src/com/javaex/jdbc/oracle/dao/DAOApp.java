1e344a25e73f9d22d6eddd9990f412408aa772db
BIT <BIT@DESKTOP-DGBBQFQ>
2021-08-10 10:49:37 +0900

    DAO INSERT

---
+++ JavaExample/src/com/javaex/jdbc/oracle/dao/DAOApp.java
@@ -0,0 +1,54 @@
+package com.javaex.jdbc.oracle.dao;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class DAOApp {
+
+	public static void main(String[] args) {
+		listAuthor();
+		
+		insertAuthor();
+		
+		listAuthor();
+
+	}
+	
+	
+	private static void insertAuthor() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("이름:");
+		String name = scanner.nextLine();
+		System.out.print("경력:");
+		String desc = scanner.nextLine();
+		
+		AuthorVO vo = new AuthorVO(name, desc);
+		
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		
+		boolean success = dao.insert(vo);
+		
+		System.out.println("Author INSERT:" + 
+				(success ? "성공": "실패"));
+		scanner.close();
+		
+	}
+	private static void listAuthor() {
+		//	DAO 생성
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		
+		List<AuthorVO> list = dao.getList();
+		Iterator<AuthorVO> it = list.iterator();
+		
+		while(it.hasNext()) {
+			AuthorVO vo = it.next();
+			System.out.printf("%d\t%s\t%s%n", 
+					vo.getAuthorId(),
+					vo.getAuthorName(),
+					vo.getAuthorDesc());
+		}
+		System.out.println("-------------------");
+	}
+
+}
